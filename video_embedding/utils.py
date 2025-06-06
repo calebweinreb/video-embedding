44c8f8290c5e60b0afeebd4cf62bdeb9e02923a0
MarieatouDaniels <marieatou.daniels@gmail.com>
2025-06-06 14:40:32 -0400

    import crop_image into utils

--- video_embedding/utils.py
+++ video_embedding/utils.py
@@ -1,32 +1,22 @@
-"""Transformation functions for video data."""
-
 import numpy as np
 import torch
-import cv2
-from scipy.ndimage import gaussian_filter1d, median_filter
-import random
-from albumentations.pytorch import ToTensorV2
-from albumentations import ReplayCompose
+from typing import List, Tuple, Union, Optional
 from vidio.read import OpenCVReader
-import albumentations as A
+import imageio
+import tqdm
 
+from.utils import crop_image
 
-def transform_video(video_array):
-    """
-    - Normalize from 0-255 to 0-1
-    - Standardize channels using hard-coded mean and std
-    - Change channel order
-    - Convert to tensor
+def transform_video(video_array: np.ndarray) -> torch.Tensor:
+    """Normalize video clip and reformat as torch tensor
 
     Args:
-        video_array (numpy.ndarray): 4D or 5D array with shape ([B], T, H, W, C), where B is the
-        batch size, T is the number of frames, H is height, W is width, and C is channels (RGB).
+        video_array: 4D or 5D array of video frames with shape ``([B], T, H, W, C)``.
 
     Returns:
-        torch.Tensor: Transformed video tensor with shape ([B], C, T, H, W)
+        Transformed video tensor with shape ``([B], C, T, H, W)``.
     """
     video_array = video_array.astype(np.float32) / 255
-
     mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
     std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
     video_array = (video_array - mean) / std
@@ -38,16 +28,14 @@ def transform_video(video_array):
     return torch.from_numpy(video_array)
 
 
-def untransform_video(video_tensor):
-    """
-    Inverts the transformations applied by the `transform_video` function.
+def untransform_video(video_tensor: torch.Tensor) -> np.ndarray:
+    """Invert the transformations applied by the `transform_video` function.
 
     Args:
-        video_tensor (torch.Tensor): Transformed video tensor with shape ([B], C, T, H, W), where B
-        is the batch size, C is channels (RGB), T is the frame count, H is height, and W is width.
+        video_tensor: Transformed video tensor with shape ``([B], C, T, H, W)``.
 
     Returns:
-        numpy.ndarray: 4D or 5D array of shape ([B], T, H, W, C) representing the original video(s).
+        4D or 5D array of shape ``([B], T, H, W, C)`` representing the original video(s).
     """
     video_array = video_tensor.numpy()
 
@@ -61,3 +49,73 @@ def untransform_video(video_tensor):
     video_array = (video_array * std) + mean
     video_array = (video_array * 255).astype(np.uint8)
     return video_array
+
+
+def sample_timepoints(
+    video_paths: List[str],
+    num_samples: int,
+    video_lengths: Optional[List[int]] = None,
+    clip_size: int = 1,
+) -> List[Tuple[str, int]]:
+    """Uniformly sample frame indexes from an ensemble of videos.
+
+    Args:
+        video_paths: List of video file paths.
+        num_samples: Number of timepoints to sample.
+        video_lengths: Video lengths in frames. If ``None``, lengths are determined from files.
+        clip_size: Ensure samples are at least this distance from the end of the video.
+
+    Returns:
+        List of tuples ``(video_path, timepoint)``.
+    """
+    if video_lengths is None:
+        video_lengths = [len(OpenCVReader(p)) for p in video_paths]
+
+    p = np.array(video_lengths) + 1 - clip_size
+    video_probabilities = p / np.sum(p)
+
+    video_indexes = np.random.choice(
+        len(video_paths), size=num_samples, p=video_probabilities
+    )
+    timepoints = [
+        np.random.randint(0, video_lengths[i] - clip_size + 1) for i in video_indexes
+    ]
+    return [(video_paths[i], t) for i, t in zip(video_indexes, timepoints)]
+
+
+def crop_video(
+    video_path: str,
+    cropped_video_path: str,
+    track: np.ndarray,
+    crop_size: Union[int, Tuple[int, int]],
+    quality: int = 5,
+    constrain_track: Optional[bool] = True,
+) -> None:
+    """Crop a video around a time-varying centroid.
+
+    Args:
+        video_path: Path to the input video.
+        cropped_video_path: Path to save the cropped video.
+        track: Array of shape ``(frames, 2)`` containing the crop centroid ``(x, y)`` for each frame.
+        crop_size: Size of the crop. If an integer is provided, it crops a square of that size.
+        quality: Quality of the output video passed to ``imageio.get_writer``.
+        constrain_track: If ``True``, ensures the cropped area does not exceed the video boundaries.
+    """
+    reader = OpenCVReader(video_path)
+
+    if not isinstance(crop_size, tuple):
+        crop_size = (crop_size, crop_size)
+
+    if constrain_track:
+        h, w = reader[0].shape[:2]
+        track[:, 0] = np.clip(track[:, 0], crop_size[0] // 2, w - crop_size[0] // 2)
+        track[:, 1] = np.clip(track[:, 1], crop_size[1] // 2, h - crop_size[1] // 2)
+
+    with imageio.get_writer(
+        cropped_video_path, pixelformat="yuv420p", fps=reader.fps, quality=quality
+    ) as writer:
+        for frame_ix in tqdm.trange(len(reader)):
+            frame = reader[frame_ix]
+            cen = track[frame_ix]
+            cropped_frame = crop_image(frame, cen, crop_size)
+            writer.append_data(cropped_frame)
--- video_embedding/visualization.py
+++ video_embedding/visualization.py
@@ -1,26 +1,28 @@
-"""Methods for visualization of videos and video embeddings."""
-
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from IPython.display import HTML
 import numpy as np
 from vidio.read import OpenCVReader
-import umap
-from sklearn.decomposition import PCA
+from typing import Optional, Dict, Tuple, Union, Iterable
+import matplotlib
+import torch
+
+from .utils import sample_timepoints, untransform_video
 
 
-def play_videos(videos, rows, cols, inches=3):
-    """
-    Play multiple videos in a grid with specified rows and columns.
+def play_videos(
+    videos: Iterable[np.ndarray], rows: int, cols: int, inches: int = 3
+) -> HTML:
+    """Play videos (arranged in a grid) in a jupyter notebook.
 
     Args:
-        videos: List of videos to display, each shaped as (frames, height, width, channels),
-        rows: Number of rows in the grid,
-        cols: Number of columns in the grid,
+        videos: List of vide clips as arrays with shape ``(frames, height, width, channels)``.
+        rows: Number of rows in the grid.
+        cols: Number of columns in the grid.
         inches: Size of each subplot in inches.
 
     Returns:
-        HTML: HTML5 video player with the specified videos.
+        HTML5 video player with the specified videos.
     """
     num_videos = len(videos)
 
@@ -68,22 +70,102 @@ def play_videos(videos, rows, cols, inches=3):
     return HTML(anim.to_html5_video())
 
 
-# def pca_and_umap(embeddings, n_components:int=20, n_neighbors:int=50):
-#   """
-#    Apply PCA and UMAP to reduce the dimensionality of embeddings.
+def crop_image(
+    image: np.ndarray, centroid: Tuple[int, int], crop_size: Union[int, Tuple[int, int]]
+) -> np.ndarray:
+    """Crop an image around a centroid.
 
-#    Args:
-#        embeddings (np.ndarray): Array of shape (n_samples, n_features) containing the embeddings.
-#        n_components (int): Number of components for PCA.
-#        n_neighbors (int): Number of neighbors for UMAP.
+    Args:
+        image: Image to crop as array of shape ``(H, W, C)`` or ``(H, W)``.
+        centroid: Tuple of ``(x, y)`` coordinates representing the centroid around which to crop.
+        crop_size: Size of the crop. If an integer is provided, it will crop a square of that size.
 
-#    Returns:
-#        np.ndarray: UMAP-reduced embeddings of shape (n_samples, 2).
-#    """
-#    pca = PCA(n_components=n_components)
-#    pca_embeddings = pca.fit_transform(embeddings)
+    Returns:
+        Cropped image as array of shape ``(H', W', C)`` or ``(H', W')``.
+    """
+    if isinstance(crop_size, tuple):
+        w, h = crop_size
+    else:
+        w, h = crop_size, crop_size
+    x, y = int(centroid[0]), int(centroid[1])
 
-#   reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=0.1, n_components=2, random_state=42)
-#    umap_embeddings = reducer.fit_transform(pca_embeddings)
+    x_min = max(0, x - w // 2)
+    y_min = max(0, y - h // 2)
+    x_max = min(image.shape[1], x + w // 2)
+    y_max = min(image.shape[0], y + h // 2)
 
-#    return umap_embeddings
+    cropped = image[y_min:y_max, x_min:x_max]
+    padded = np.zeros((h, w, *image.shape[2:]), dtype=image.dtype)
+    pad_x = max(w // 2 - x, 0)
+    pad_y = max(h // 2 - y, 0)
+    padded[pad_y : pad_y + cropped.shape[0], pad_x : pad_x + cropped.shape[1]] = cropped
+    return padded
+
+
+def inspect_crop_sizes(
+    tracks: Dict[str, np.ndarray],
+    inner_crop_size: int,
+    outer_crop_size: int,
+    n_examples: Optional[int] = 4,
+) -> matplotlib.figure.Figure:
+    """Visualize crop sizes for a random sample of frames.
+
+    Args:
+        tracks: Dict from video paths to tracks containing the animal's centroid at each frame.
+        inner_crop_size: Size of the inner crop.
+        outer_crop_size: Size of the outer crop.
+        n_examples: Number of examples to visualize.
+
+    Returns:
+        Figure containing the visualizations.
+    """
+    video_paths = list(tracks.keys())
+    samples = sample_timepoints(video_paths, n_examples)
+    box = np.array([[-1, -1, 1, 1, -1], [-1, 1, 1, -1, -1]]).T / 2
+
+    fig, axes = plt.subplots(2, n_examples, sharey="row")
+    for i, (video_path, frame_ix) in enumerate(samples):
+        frame = OpenCVReader(video_path)[frame_ix]
+        cen = tracks[video_path][frame_ix]
+
+        # full frame
+        axes[0, i].imshow(frame)
+        axes[0, i].plot(*(box * outer_crop_size + cen).T, c="b", lw=2)
+        axes[0, i].plot(*(box * inner_crop_size + cen).T, c="b", lw=2, ls="--")
+
+        # cropped frame
+        cropped_frame = crop_image(frame, cen, outer_crop_size)
+        axes[1, i].imshow(cropped_frame)
+        axes[1, i].plot(
+            *(box * inner_crop_size + outer_crop_size / 2).T, c="b", lw=2, ls="--"
+        )
+
+    for ax in axes.flat:
+        ax.set_facecolor("black")
+
+    return fig
+
+
+def inspect_dataloader(
+    dataloader: Union[torch.utils.data.DataLoader, Iterable],
+    num_samples: int = 4,
+    inches: int = 3,
+) -> HTML:
+    """Visualize a batch of augmented video clip pairs from a dataloader.
+
+    Args:
+        dataloader: Dataloader or iterable yielding batched pairs of augmented video clips.
+        num_samples: Number of samples to visualize.
+        inches: Size of each subplot in inches.
+
+    Returns:
+        HTML5 video player displaying the video clips.
+    """
+    x_one, x_two = next(iter(dataloader))
+    if x_one.shape[0] < num_samples:
+        raise ValueError(
+            f"Batch size {x_one.shape[0]} is less than requested number of samples {num_samples}."
+        )
+    x_one = untransform_video(x_one)[:num_samples]
+    x_two = untransform_video(x_two)[:num_samples]
+    return play_videos(np.concatenate([x_one, x_two]), 2, num_samples, inches)
